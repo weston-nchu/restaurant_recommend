92e3d4975c26fa360d79d2e1f44221b7cb731dac
Weston <lovemow1121@gmail.com>
2025-06-05 21:54:26 +0800

    新增DQN

--- library/dao/restaurant_dao.py
+++ library/dao/restaurant_dao.py
@@ -1,8 +1,13 @@
+from typing import List
 from pyfk import CrudSql
 from sqlalchemy.orm.session import Session
 from .schema.restaurant import Restaurant
+from sqlalchemy import select
 
 class RestaurantDao(CrudSql[Restaurant]):
 
     def __init__(self, db: Session = None):
         super().__init__(db)
+
+    def query_code_by_uid_list(self, uid_list: List[str]) -> List[str]:
+        return [obj[0] for obj in self.db.query(Restaurant.code).filter(Restaurant.uid.in_(uid_list)).all()]
--- routers/learning_router.py
+++ routers/learning_router.py
@@ -1,8 +1,8 @@
-import random
+from typing import List
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
-
+from library import dqn
 from library.dao import RestaurantDao
 
 router = APIRouter(
@@ -18,17 +18,31 @@ async def index(request: Request):
 
 @router.get("/restaurant", description="取得3筆餐廳資料")
 async def restaurant():
+    restr_codes_batch = dqn.start()
+    dao = RestaurantDao()
     result_list = []
-    restaurants = RestaurantDao().get_list()
-    for restr in random.sample(restaurants, 3):
-        result_list.append({
-            "uid": restr.uid,
-            "name": restr.name,
-            "desc": restr.desc,
-            "images": restr.get_img_url_list()
-        })
+    for restr_codes in restr_codes_batch:
+        restr_list = []
+        for code in list(restr_codes.values()):
+            restr = dao.get_one(code=code)
+            restr_list.append({
+                "uid": restr.uid,
+                "name": restr.name,
+                "desc": restr.desc,
+                "images": restr.get_img_url_list()
+            })
+        result_list.append(restr_list)
 
     return JSONResponse(content={
         "message": "Success",
         "content": result_list
+    })
+
+@router.post("/train", description="接收用戶選擇餐廳並開始學習")
+async def train(restr_list: List[str]):
+    code_list = RestaurantDao().query_code_by_uid_list(restr_list)
+    dqn.train(code_list)
+    return JSONResponse(content={
+        "message": "Success",
+        "content": ""
     })
